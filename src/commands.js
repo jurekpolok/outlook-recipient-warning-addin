e411aac2bf4b5af8edbad6e760146195219e46ce
Jurek Polok <jurek@Jureks-MacBook-Air-7.local>
2025-12-10 15:40:06 +0100

    Add Office.onReady back - required for Outlook Web

--- src/commands.js
+++ src/commands.js
@@ -1,36 +1,12 @@
 /* global Office */
 
-// Configuration
-var RECIPIENT_THRESHOLD = 5;
-var INTERNAL_DOMAINS = [
-    "bcc.no",
-    "bcc.media",
-    "brunstad.tv",
-    "bccyep.no",
-    "bcc-crew.com"
-];
-
-function isExternalEmail(email) {
-    if (!email) return false;
-    var emailLower = email.toLowerCase();
-    for (var i = 0; i < INTERNAL_DOMAINS.length; i++) {
-        if (emailLower.indexOf("@" + INTERNAL_DOMAINS[i].toLowerCase()) !== -1) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function getEmailAddress(recipient) {
-    return recipient.emailAddress || recipient.address || "";
-}
+Office.onReady(function() {
+    // Office is ready
+});
 
 function onMessageSendHandler(event) {
-    // Simple test - just show warning for any email
-    event.completed({
-        allowEvent: false,
-        errorMessage: "Test: Add-in is working! You have recipients in your email."
-    });
+    // Just allow the send immediately - test if event.completed works
+    event.completed({ allowEvent: true });
 }
 
 Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
